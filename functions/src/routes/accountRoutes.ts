64e7f3f8e1056e833e578c500f47e4f0dc3c64fc
Rocha000 <lucca_laxusbr15@hotmail.com>
2024-11-19 22:54:49 -0300

    Controller e route da conta criadas

--- functions/src/routes/accountRoutes.ts
+++ functions/src/routes/accountRoutes.ts
@@ -1,6 +1,8 @@
 /* eslint-disable max-len */
 import * as functions from "firebase-functions";
 import { createExpense, updateExpense } from "../controllers/expenseController";
+import { createAccount, deleteAccount, getAccount, getAllAccounts, updateAccount } from "../controllers/accountController";
+
 
 // Exporta as rotas de contas (Account)
 export const accountRoutes = {
@@ -9,5 +11,20 @@ export const accountRoutes = {
   ),
   updateExpense: functions.https.onCall(
     async (request) => await updateExpense(request)
+  ),
+  createAccount: functions.https.onCall(
+    async (request) => await createAccount(request)
+  ),
+  updateAccount: functions.https.onCall(
+    async (request) => await updateAccount(request)
+  ),
+  deleteAccount: functions.https.onCall(
+    async (request) => await deleteAccount(request)
+  ),
+  getAccount: functions.https.onCall(
+    async (request) => await getAccount(request)
+  ),
+  getAllAccounts: functions.https.onCall(
+    async (request) => await getAllAccounts(request)
   ),
 };
